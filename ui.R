9e405255a9c813cf3ee053b2bfe08415ad468a34
Chris Owyang <chris.owyang@gmail.com>
2016-02-21 19:44:11 -0500

    debugging v1

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ shinyUI(fluidPage(
             fileInput('file1','Choose CSV File',accept=c('text/csv','text/comma-separated-values','text/plain','.csv')),
             tags$hr(),
             checkboxInput('header','Header',TRUE),
-            radioButtons('sep','Separator',c(Comma=',',Semicolon';',Tab='\t'),','),
+            radioButtons('sep','Separator',c(Comma=',',Semicolon=';',Tab='\t'),','),
             radioButtons('quote','Quote',c(None='','Double Quote'='"','Single Quote'="'"),'"'),
             selectInput("type",label="Model Type", choices=list('Linear Model'='Linear Model'),selected='Linear Model'),
             textInput("model",label="Enter model",value="y~x1+x2+..."),
@@ -18,10 +18,8 @@ shinyUI(fluidPage(
             selectInput("plotType",label="Plot Type",choices=list('Response Transformation'='Response Transformation','Correlation Heat Map'='Correlation Heat Map','Classical MDS'='Classical MDS'),selected='Response Transformation')),
             mainPanel(plotOutput("plots",click="plot1_click",brush=brush0pts (id="plot1_brush"),width="650px",height="650px"))
         ),
-            #fluidRow(column(width=6,h3("Points near click"),
-            verbatimTextOutput("click_info")),
-                #column(width=6,h3("Brushed points"),
-                verbatimTextOutput("brush_info")),
+            #fluidRow(column(width=6,h3("Points near click"),verbatimTextOutput("click_info")),
+                #column(width=6,h3("Brushed points"),verbatimTextOutput("brush_info")),
             #hr(),
         fluidRow(column(4,verbatimTextOutput('contents')),column(4,verbatimTextOutput('summary')))
 ))
